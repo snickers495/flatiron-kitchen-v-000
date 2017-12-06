5a577d6416ef58ffcb46ac485201e4f2f0020ce1
Lisa Lee <snickers495@gmail.com>
2017-12-06 15:20:33 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,19 @@
 class RecipesController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end 
 end
