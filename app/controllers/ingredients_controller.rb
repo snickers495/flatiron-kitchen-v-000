ad149c82d00eae9fb8269fe04bdf5d656740d71d
Lisa Lee <snickers495@gmail.com>
2017-12-06 15:10:46 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -5,11 +5,12 @@ class IngredientsController < ApplicationController
   end
 
   def edit
+    @ingredient = Ingredient.find(params[:id])
   end
 
   private
 
   def ingredient_params
     params.require(:ingredient).permit(:name)
-  end 
+  end
 end
