8d9b6c029508e0f61c710f3bf6aeca13a910f281
Lisa Lee <snickers495@gmail.com>
2017-12-06 15:24:19 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: [:show, :edit, :delete] 
+  before_action :set_recipe, only: [:show, :edit, :update] 
   def show
   end
 
