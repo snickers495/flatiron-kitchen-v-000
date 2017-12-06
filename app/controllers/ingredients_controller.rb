fdf074d17c411ca0d3fc29efeec4ebc8e99e455a
Lisa Lee <snickers495@gmail.com>
2017-12-06 15:10:16 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,14 @@
 class IngredientsController < ApplicationController
+
+  def new 
+  end 
+
+  def edit 
+  end 
+
+  private 
+
+  def ingredient_params 
+    params.require(:ingredient).permit(:name)
+  end 
 end
